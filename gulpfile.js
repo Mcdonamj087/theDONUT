01ee12c629be2ffb3d2b4ee6f250d562289d5378
Matt McDonald <mcdonamj087@gmail.com>
2020-08-18 11:46:27 -0500

    Finalized scss and nunjucks components restructure

--- gulpfile.js
+++ gulpfile.js
@@ -4,113 +4,115 @@
  */
 
 var settings = {
-	clean: true,
-	scripts: true,
-	polyfills: false,
-	styles: true,
-	svgs: true,
-	copy: true,
-	reload: true
+  clean: true,
+  scripts: true,
+  polyfills: false,
+  styles: true,
+  svgs: true,
+  copy: true,
+  reload: true,
 };
-
 
 /**
  * Paths to project folders
  */
 
 var paths = {
-	input: 'src/',
-	output: 'dist/',
-	scripts: {
-		input: 'src/js/*',
-		polyfills: '.polyfill.js',
-		output: 'dist/js/'
-	},
-	styles: {
-		input: 'src/scss/**/*.{scss,sass}',
-		output: 'dist/css/'
-	},
-	nunjucks: {
-		input: 'src/nunjucks/pages/**/*.+(html|nunjucks)',
-		output: 'dist/',
-		templates: 'src/nunjucks/templates'
-	},
-	svgs: {
-		input: 'src/svg/*.svg',
-		output: 'dist/svg/'
-	},
-	copy: {
-		css: {
-			input: 'src/copy/css/**/*',
-			output: 'dist/css/'
-		},
-		js: {
-			input: 'src/copy/plugins/**/*',
-			output: 'dist/js/plugins/'
-		},
-		fonts: {
-			input: 'src/copy/fonts/**/*',
-			output: 'dist/fonts/'
-		}, 
-		images: {
-			input: 'src/copy/images/**/*',
-			output: 'dist/images/'
-		},
-		root: {
-			input: 'src/copy/root/**/*',
-			output: 'dist/'
-		}
-	},
-	reload: './dist/',
-	find_replace: {
-		input: 'dist/**/*',
-		find: 'http://localhost:3000',
-		replace_local: 'http://localhost:8888/theDONUT/wp-content/themes/theDONUT',
-		replace_live: 'https://thedonut.co/wp-content/themes/theDONUT',
-		replace_live_ambassadors: 'https://ambassadors.thedonut.co/wp-content/themes/theDONUT'
-	}
+  input: 'src/',
+  output: 'dist/',
+  scripts: {
+    input: 'src/js/*',
+    polyfills: '.polyfill.js',
+    output: 'dist/js/',
+  },
+  styles: {
+    input: 'src/scss/**/*.{scss,sass}',
+    output: 'dist/css/',
+  },
+  nunjucks: {
+    input: 'src/nunjucks/pages/**/*.+(html|nunjucks)',
+    output: 'dist/',
+    templates: 'src/nunjucks/templates',
+  },
+  svgs: {
+    input: 'src/svg/*.svg',
+    output: 'dist/svg/',
+  },
+  copy: {
+    css: {
+      input: 'src/copy/css/**/*',
+      output: 'dist/css/',
+    },
+    js: {
+      input: 'src/copy/plugins/**/*',
+      output: 'dist/js/plugins/',
+    },
+    fonts: {
+      input: 'src/copy/fonts/**/*',
+      output: 'dist/fonts/',
+    },
+    images: {
+      input: 'src/copy/images/**/*',
+      output: 'dist/images/',
+    },
+    root: {
+      input: 'src/copy/root/**/*',
+      output: 'dist/',
+    },
+  },
+  reload: './dist/',
+  find_replace: {
+    input: 'dist/**/*',
+    find: 'http://localhost:3000',
+    replace_local: 'http://localhost:8888/theDONUT/wp-content/themes/theDONUT',
+    replace_live: 'https://thedonut.co/wp-content/themes/theDONUT',
+    replace_live_ambassadors:
+      'https://ambassadors.thedonut.co/wp-content/themes/theDONUT',
+  },
 };
-
 
 /**
  * Template for banner to add to file headers
  */
 
 var banner = {
-	full:
-		'/*!\n' +
-		' * <%= package.name %> v<%= package.version %>\n' +
-		' * <%= package.description %>\n' +
-		' * (c) ' + new Date().getFullYear() + ' <%= package.author.name %>\n' +
-		' * <%= package.license %> License\n' +
-		' * <%= package.repository.url %>\n' +
-		' */\n\n',
-	min:
-		'/*!' +
-		' <%= package.name %> v<%= package.version %>' +
-		' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
-		' | <%= package.license %> License' +
-		' | <%= package.repository.url %>' +
-		' */\n',
-	wp:
-		'/*\n' +
-		'Theme Name: the DONUT\n' +
-		'Author: Matt McDonald\n' +
-		'Author URI: mattmcdonalddesign.com\n' +
-		'Description: the DONUT\n' +
-		'Version: 1.0\n' +
-		'License: GNU General Public License v2 or later\n' +
-		'License URI: http://www.gnu.org/licenses/gpl-2.0.html\n' +
-		'*/\n\n'
+  full:
+    '/*!\n' +
+    ' * <%= package.name %> v<%= package.version %>\n' +
+    ' * <%= package.description %>\n' +
+    ' * (c) ' +
+    new Date().getFullYear() +
+    ' <%= package.author.name %>\n' +
+    ' * <%= package.license %> License\n' +
+    ' * <%= package.repository.url %>\n' +
+    ' */\n\n',
+  min:
+    '/*!' +
+    ' <%= package.name %> v<%= package.version %>' +
+    ' | (c) ' +
+    new Date().getFullYear() +
+    ' <%= package.author.name %>' +
+    ' | <%= package.license %> License' +
+    ' | <%= package.repository.url %>' +
+    ' */\n',
+  wp:
+    '/*\n' +
+    'Theme Name: the DONUT\n' +
+    'Author: Matt McDonald\n' +
+    'Author URI: mattmcdonalddesign.com\n' +
+    'Description: the DONUT\n' +
+    'Version: 1.0\n' +
+    'License: GNU General Public License v2 or later\n' +
+    'License URI: http://www.gnu.org/licenses/gpl-2.0.html\n' +
+    '*/\n\n',
 };
-
 
 /**
  * Gulp Packages
  */
 
 // General
-var {gulp, src, dest, watch, series, parallel} = require('gulp');
+var { gulp, src, dest, watch, series, parallel } = require('gulp');
 var del = require('del');
 var fs = require('fs');
 var gulpData = require('gulp-data');
@@ -142,268 +144,245 @@ var browserSync = require('browser-sync');
 // Nunjucks
 var nunjucksRender = require('gulp-nunjucks-render');
 
-
 /**
  * Gulp Tasks
  */
 
 // Remove pre-existing content from output folders
 var cleanDist = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.clean) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.clean) return done();
+  // Clean the dist folder
+  del.sync([paths.output]);
 
-	// Clean the dist folder
-	del.sync([
-		paths.output
-	]);
-
-	// Signal completion
-	return done();
-
+  // Signal completion
+  return done();
 };
 
 // Repeated JavaScript tasks
 var jsTasks = lazypipe()
-	.pipe(header, banner.full, {package: package})
-	.pipe(optimizejs)
-	.pipe(dest, paths.scripts.output)
-	.pipe(rename, {suffix: '.min'})
-	.pipe(uglify)
-	.pipe(optimizejs)
-	.pipe(header, banner.min, {package: package})
-	.pipe(dest, paths.scripts.output);
+  .pipe(header, banner.full, { package: package })
+  .pipe(optimizejs)
+  .pipe(dest, paths.scripts.output)
+  .pipe(rename, { suffix: '.min' })
+  .pipe(uglify)
+  .pipe(optimizejs)
+  .pipe(header, banner.min, { package: package })
+  .pipe(dest, paths.scripts.output);
 
 // Lint, minify, and concatenate scripts
 var buildScripts = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.scripts) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.scripts) return done();
+  // Run tasks on script files
+  return src(paths.scripts.input).pipe(
+    flatmap(function (stream, file) {
+      // If the file is a directory
+      if (file.isDirectory()) {
+        // Setup a suffix variable
+        var suffix = '';
 
-	// Run tasks on script files
-	return src(paths.scripts.input)
-		.pipe(flatmap(function(stream, file) {
+        // If separate polyfill files enabled
+        if (settings.polyfills) {
+          // Update the suffix
+          suffix = '.polyfills';
 
-			// If the file is a directory
-			if (file.isDirectory()) {
+          // Grab files that aren't polyfills, concatenate them, and process them
+          src([
+            file.path + '/*.js',
+            '!' + file.path + '/*' + paths.scripts.polyfills,
+          ])
+            .pipe(concat(file.relative + '.js'))
+            .pipe(jsTasks());
+        }
 
-				// Setup a suffix variable
-				var suffix = '';
+        // Grab all files and concatenate them
+        // If separate polyfills enabled, this will have .polyfills in the filename
+        src(file.path + '/*.js')
+          .pipe(concat(file.relative + suffix + '.js'))
+          .pipe(jsTasks());
 
-				// If separate polyfill files enabled
-				if (settings.polyfills) {
+        return stream;
+      }
 
-					// Update the suffix
-					suffix = '.polyfills';
-
-					// Grab files that aren't polyfills, concatenate them, and process them
-					src([file.path + '/*.js', '!' + file.path + '/*' + paths.scripts.polyfills])
-						.pipe(concat(file.relative + '.js'))
-						.pipe(jsTasks());
-
-				}
-
-				// Grab all files and concatenate them
-				// If separate polyfills enabled, this will have .polyfills in the filename
-				src(file.path + '/*.js')
-					.pipe(concat(file.relative + suffix + '.js'))
-					.pipe(jsTasks());
-
-				return stream;
-
-			}
-
-			// Otherwise, process the file
-			return stream.pipe(jsTasks());
-
-		}));
-
+      // Otherwise, process the file
+      return stream.pipe(jsTasks());
+    })
+  );
 };
 
 // Lint scripts
 var lintScripts = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.scripts) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.scripts) return done();
-
-	// Lint scripts
-	return src(paths.scripts.input)
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'));
-
+  // Lint scripts
+  return src(paths.scripts.input)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
 };
 
 // Process, lint, and minify Sass files
 var buildStyles = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.styles) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.styles) return done();
-
-	// Run tasks on all Sass files
-	return src(paths.styles.input, { sourcemaps: true })
-		.pipe(sass({
-			outputStyle: 'expanded',
-			sourceComments: false
-		}))
-		.pipe(prefix({
-			cascade: true,
-			remove: true
-		}))
-		.pipe(header(banner.wp, { package : package }))
-		.pipe(dest(paths.styles.output))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minify({
-			discardComments: {
-				removeAll: true
-			}
-		}))
-		.pipe(header(banner.min, { package : package }))
-		.pipe(dest(paths.styles.output));
-
+  // Run tasks on all Sass files
+  return src(paths.styles.input, { sourcemaps: true })
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+        sourceComments: false,
+      })
+    )
+    .pipe(
+      prefix({
+        cascade: true,
+        remove: true,
+      })
+    )
+    .pipe(header(banner.wp, { package: package }))
+    .pipe(dest(paths.styles.output))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(
+      minify({
+        discardComments: {
+          removeAll: true,
+        },
+      })
+    )
+    .pipe(header(banner.min, { package: package }))
+    .pipe(dest(paths.styles.output));
 };
 
-
-
-
-var nunjucks = function(done) {
-	return src(paths.nunjucks.input)
-		.pipe(gulpData(function(){
-			return JSON.parse(fs.readFileSync(`./${paths.input}nunjucks/data.json`))
-		}))
-		.pipe(nunjucksRender({
-			path: paths.nunjucks.templates
-		}))
-		.pipe(dest(paths.nunjucks.output))
+var nunjucks = function (done) {
+  return src(paths.nunjucks.input)
+    .pipe(
+      gulpData(function () {
+        return JSON.parse(
+          fs.readFileSync(`./${paths.input}nunjucks/data.json`)
+        );
+      })
+    )
+    .pipe(
+      nunjucksRender({
+        path: paths.nunjucks.templates,
+      })
+    )
+    .pipe(dest(paths.nunjucks.output));
 };
 
 // Optimize SVG files
 var buildSVGs = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.svgs) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.svgs) return done();
-
-	// Optimize SVG files
-	return src(paths.svgs.input)
-		.pipe(svgmin())
-		.pipe(dest(paths.svgs.output));
-
+  // Optimize SVG files
+  return src(paths.svgs.input).pipe(svgmin()).pipe(dest(paths.svgs.output));
 };
 
 // Copy static CSS files into output folder
 var copyCSS = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.copy) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.copy) return done();
-
-	// Copy CSS files
-	return src(paths.copy.css.input)
-		.pipe(dest(paths.copy.css.output));
-
+  // Copy CSS files
+  return src(paths.copy.css.input).pipe(dest(paths.copy.css.output));
 };
 
 // Copy static JS files into output folder
 var copyJS = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.copy) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.copy) return done();
-
-	// Copy JS files
-	return src(paths.copy.js.input)
-		.pipe(dest(paths.copy.js.output));
-
+  // Copy JS files
+  return src(paths.copy.js.input).pipe(dest(paths.copy.js.output));
 };
 
 // Copy font files into output folder
 var copyFonts = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.copy) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.copy) return done();
-
-	// Copy Font files
-	return src(paths.copy.fonts.input)
-		.pipe(dest(paths.copy.fonts.output));
-
+  // Copy Font files
+  return src(paths.copy.fonts.input).pipe(dest(paths.copy.fonts.output));
 };
 
 // Copy images into output folder
 var copyImages = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.copy) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.copy) return done();
-
-	// Copy Image files
-	return src(paths.copy.images.input)
-		.pipe(dest(paths.copy.images.output));
-
+  // Copy Image files
+  return src(paths.copy.images.input).pipe(dest(paths.copy.images.output));
 };
 
 // Copy files into the root dist folder
 var copyRootFiles = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.copy) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.copy) return done();
-
-	// Copy Image files
-	return src(paths.copy.root.input)
-		.pipe(dest(paths.copy.root.output));
-
+  // Copy Image files
+  return src(paths.copy.root.input).pipe(dest(paths.copy.root.output));
 };
-
-
 
 // Watch for changes to the src directory
 var startServer = function (done) {
+  // Make sure this feature is activated before running
+  if (!settings.reload) return done();
 
-	// Make sure this feature is activated before running
-	if (!settings.reload) return done();
+  // Initialize BrowserSync
+  browserSync.init({
+    server: {
+      baseDir: paths.reload,
+    },
+    browser: 'Firefox Developer Edition',
+  });
 
-	// Initialize BrowserSync
-	browserSync.init({
-		server: {
-			baseDir: paths.reload
-		},
-		browser: "Firefox Developer Edition"
-	});
-
-	// Signal completion
-	done();
-
+  // Signal completion
+  done();
 };
 
 // Reload the browser when files change
 var reloadBrowser = function (done) {
-	if (!settings.reload) return done();
-	browserSync.reload();
-	done();
+  if (!settings.reload) return done();
+  browserSync.reload();
+  done();
 };
 
 // Watch for changes
 var watchSource = function (done) {
-	watch(paths.input, series(exports.default, reloadBrowser));
-	done();
+  watch(paths.input, series(exports.default, reloadBrowser));
+  done();
 };
 
 // Find and replace root url in dist folder for local WP
 function replaceLocal() {
-	return src(paths.find_replace.input)
-		.pipe(replace(paths.find_replace.find, paths.find_replace.replace_local))
-		.pipe(dest(paths.copy.root.output));
-};
+  return src(paths.find_replace.input)
+    .pipe(replace(paths.find_replace.find, paths.find_replace.replace_local))
+    .pipe(dest(paths.copy.root.output));
+}
 
 // Find and replace root url in dist folder for thedonut.co
 function replaceLive() {
-	return src(paths.find_replace.input)
-		.pipe(replace(paths.find_replace.find, paths.find_replace.replace_live))
-		.pipe(dest(paths.copy.root.output));
-};
+  return src(paths.find_replace.input)
+    .pipe(replace(paths.find_replace.find, paths.find_replace.replace_live))
+    .pipe(dest(paths.copy.root.output));
+}
 
 // Find and replace root url in dist folder for thedonut.co
 function replaceLiveAmbassadors() {
-	return src(paths.find_replace.input)
-		.pipe(replace(paths.find_replace.find, paths.find_replace.replace_live_ambassadors))
-		.pipe(dest(paths.copy.root.output));
-};
-
+  return src(paths.find_replace.input)
+    .pipe(
+      replace(
+        paths.find_replace.find,
+        paths.find_replace.replace_live_ambassadors
+      )
+    )
+    .pipe(dest(paths.copy.root.output));
+}
 
 /**
  * Export Tasks
@@ -411,35 +390,19 @@ function replaceLiveAmbassadors() {
 
 // Copy Task
 // gulp copy
-exports.copy = series(
-	copyCSS,
-	copyJS,
-	copyFonts,
-	copyImages,
-	copyRootFiles
-);
+exports.copy = series(copyCSS, copyJS, copyFonts, copyImages, copyRootFiles);
 
 // Default task
 // gulp
 exports.default = series(
-	cleanDist,
-	nunjucks,
-	parallel(
-		buildScripts,
-		lintScripts,
-		buildStyles,
-		buildSVGs,
-		exports.copy
-	)
+  cleanDist,
+  nunjucks,
+  parallel(buildScripts, lintScripts, buildStyles, buildSVGs, exports.copy)
 );
 
 // Watch and reload
 // gulp watch
-exports.watch = series(
-	exports.default,
-	startServer,
-	watchSource
-);
+exports.watch = series(exports.default, startServer, watchSource);
 
 exports.replace_local = replaceLocal;
 exports.replace_live = replaceLive;
--- src/js/main/main.js
+++ src/js/main/main.js
@@ -10,39 +10,44 @@ function openMobileSubmenu() {
   this.classList.toggle('is-open');
 }
 
-mobileMenuNavItem.forEach( item => item.addEventListener('click', openMobileSubmenu));
+mobileMenuNavItem.forEach(item =>
+  item.addEventListener('click', openMobileSubmenu)
+);
 
 hamburger.addEventListener('click', e => {
-	body.classList.toggle('mobile-nav-is-open');
-  body.classList.contains('mobile-nav-is-open') ? bodyScrollLock.disableBodyScroll(mobileNavWrapper) : bodyScrollLock.enableBodyScroll(mobileNavWrapper);
+  body.classList.toggle('mobile-nav-is-open');
+  body.classList.contains('mobile-nav-is-open')
+    ? bodyScrollLock.disableBodyScroll(mobileNavWrapper)
+    : bodyScrollLock.enableBodyScroll(mobileNavWrapper);
 });
 
 const mq = window.matchMedia('(min-width: 992px)');
 
 function normalize() {
   // Hide Mobile Nav if it's open when user reduces browser width
-  if(mq.matches && body.classList.contains('mobile-nav-is-open')) {
+  if (mq.matches && body.classList.contains('mobile-nav-is-open')) {
     body.classList.remove('mobile-nav-is-open');
     bodyScrollLock.enableBodyScroll(mobileNavWrapper);
-  } 
+  }
 
-  if(!mq.matches) {
+  if (!mq.matches) {
     header.classList.remove('form-active');
   }
 }
 
-headerCTA.addEventListener('click', function(e){
+headerCTA.addEventListener('click', function (e) {
   e.preventDefault();
   header.classList.add('form-active');
-})
+});
 
-window.addEventListener('resize', _.throttle(normalize, 100, {trailing: false}));
+window.addEventListener(
+  'resize',
+  _.throttle(normalize, 100, { trailing: false })
+);
 
 function isMobileNavVisible(el) {
   return el.classList.contains('mobile-nav-is-open') ? true : false;
 }
-
-
 
 /*****************************************
   Inject UTM params into CM custom fields
@@ -55,20 +60,140 @@ function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
   var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
   var results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
+  return results === null
+    ? ''
+    : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
 const subForms = document.querySelectorAll('#subForm');
 
-if(subForms) {
+if (subForms) {
   // console.log('form(s) exists!')
   const utmSource = getUrlParameter('utm_source');
   const utmCampaign = getUrlParameter('utm_campaign');
-  const referrer = document.referrer == "" ? 'Direct' : document.referrer;
+  const referrer = document.referrer == '' ? 'Direct' : document.referrer;
 
-  subForms.forEach( form => {
+  subForms.forEach(form => {
     form.querySelector('input[name=cm-f-yhhllyd]').value = utmSource;
     form.querySelector('input[name=cm-f-ykqhyi]').value = utmCampaign;
     form.querySelector('input[name=cm-f-ykfdjl]').value = referrer;
   });
 }
+
+const accordions = document.querySelectorAll('.accordion');
+
+// if (accordions.length) {
+//   accordions.forEach(accordion => {
+//     const q = accordion.querySelector('.question');
+//     const a = accordion.querySelector('.answer');
+
+//     if (accordion.getAttribute('data-expanded') == 'false') {
+//       accordion.style.height = `${q.scrollHeight}px`;
+//     } else {
+//       accordion.classList.add('is-open');
+//     }
+
+//     function expand() {
+//       a.style.display = 'block';
+//       accordion.style.height = `${a.scrollHeight}px`;
+//       requestAnimationFrame(function () {
+//         accordion.style.height = `${accordion.scrollHeight}px`;
+//       });
+//       accordion.addEventListener('transitionend', function (e) {
+//         accordion.style.height = 'auto';
+//         accordion.removeEventListener('transitionend', arguments.callee);
+//       });
+//       accordion.classList.add('is-open');
+//       accordion.setAttribute('data-expanded', 'true');
+//     }
+
+//     function collapse() {
+//       accordion.style.height = `${accordion.scrollHeight}px`;
+
+//       requestAnimationFrame(function () {
+//         accordion.style.height = `${q.scrollHeight}px`;
+//         accordion.addEventListener('transitionend', function (e) {
+//           a.style.display = 'none';
+//           accordion.style.height = 'auto';
+//           accordion.removeEventListener('transitionend', arguments.callee);
+//         });
+//       });
+//       accordion.classList.remove('is-open');
+//       accordion.setAttribute('data-expanded', 'false');
+//     }
+
+//     q.addEventListener('click', function () {
+//       if (accordion.getAttribute('data-expanded') == 'true') {
+//         requestAnimationFrame(collapse);
+//       } else {
+//         requestAnimationFrame(expand);
+//       }
+//     });
+//   });
+// }
+
+class Accordion {
+  constructor(el) {
+    this.instance = el;
+    this.question = this.instance.querySelector('.question');
+    this.answer = this.instance.querySelector('.answer');
+    this.question.addEventListener('click', () => {
+      if (this.instance.getAttribute('data-expanded') == 'true') {
+        this.collapse();
+        console.log('collapse');
+      } else {
+        this.expand();
+        console.log('expand');
+      }
+    });
+    this.instance.addEventListener('transitionend', e => {
+      if (e.propertyName === 'height') {
+        if (this.instance.dataset.expanded === 'true') {
+          console.log('expanding finished');
+          this.instance.style.height = 'auto';
+        } else {
+          console.log('collapsing finished');
+          this.answer.style.display = 'none';
+          requestAnimationFrame(() => {
+            this.instance.style.height = 'auto';
+          });
+        }
+      }
+    });
+  }
+
+  init() {
+    if (this.instance.getAttribute('data-expanded') === 'false') {
+      this.instance.style.height = `${this.question.scrollHeight}px`;
+      this.answer.style.display = 'none';
+      // this.collapse();
+    } else {
+      this.instance.classList.add('is-open');
+    }
+  }
+
+  expand() {
+    this.answer.style.display = 'block';
+    this.instance.style.height = `${this.question.scrollHeight}px`;
+    requestAnimationFrame(() => {
+      this.instance.style.height = `${this.instance.scrollHeight}px`;
+    });
+    this.instance.classList.add('is-open');
+    this.instance.dataset.expanded = 'true';
+  }
+
+  collapse() {
+    this.instance.style.height = `${this.instance.scrollHeight}px`;
+    requestAnimationFrame(() => {
+      this.instance.style.height = `${this.question.scrollHeight}px`;
+    });
+
+    this.instance.classList.remove('is-open');
+    this.instance.dataset.expanded = 'false';
+  }
+}
+
+accordions.forEach(item => {
+  const accordion = new Accordion(item);
+  accordion.init();
+});
